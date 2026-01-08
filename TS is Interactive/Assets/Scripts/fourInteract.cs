16da8698ee825b516eb27f361594e396d9f10dbd
nemaR5133 <145371930+nemaR5133@users.noreply.github.com>
2026-01-08 09:09:02 -0800

    made a script but it doesnt do anything yet

--- TS is Interactive/Assets/Scripts/fourInteract.cs
+++ TS is Interactive/Assets/Scripts/fourInteract.cs
@@ -1,7 +1,21 @@
+/*******************************************************************
+ * File name: fourInteract
+ * Author: Nathen Mattis
+ * Digipen Email: 1119065@lwsd.org
+ * Course: Video Game Programming I
+ * Last edited: 1/8/2026
+ *
+ * Description: Prefixed with "four" to denote which scene this script
+ * is for. This script is attached to the player to figure out when
+ * they are able to "use AI," sending a signal to another script to
+ * put the "AI's" response on the screen
+ ********************************************************************/
 using UnityEngine;
 
 public class fourInteract : MonoBehaviour
 {
+    public bool canInteract;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,11 +25,24 @@ public class fourInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("enter triggered");
+        /*if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("interact");
+        }*/
+        //Debug.Log("enter triggered");
+
+    }
+
+    private void processInputs()
+    {
+
     }
 }
