943a451d94c42ce8bbddeaab9755ef3f87934fdf
codingGrease <jackson.leclaire@digipen.edu>
2026-01-08 09:09:48 -0800

    We have some textures
    There they are

--- TS is Interactive/Assets/Scripts/Glasses Rotate.cs
+++ TS is Interactive/Assets/Scripts/Glasses Rotate.cs
@@ -5,6 +5,6 @@ public class ExampleClass : MonoBehaviour
     void Update()
     {
         // Rotate the object around its local y axis at 1 degree per second
-        transform.Rotate(Vector3.up * 2 * Time.deltaTime);
+        transform.Rotate((Vector3.up * 50) * Time.deltaTime);
     }
 }
