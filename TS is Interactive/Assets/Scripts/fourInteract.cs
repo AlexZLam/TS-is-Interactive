3513d62750ebb37fc170ded5064d1467137efb75
nemaR5133 <145371930+nemaR5133@users.noreply.github.com>
2026-01-09 08:34:48 -0800

    interact script
    may or may not be totally discarded because we are centralizing

--- TS is Interactive/Assets/Scripts/fourInteract.cs
+++ TS is Interactive/Assets/Scripts/fourInteract.cs
@@ -8,27 +8,38 @@
  * Description: Prefixed with "four" to denote which scene this script
  * is for. This script is attached to the player to figure out when
  * they are able to "use AI," sending a signal to another script to
- * put the "AI's" response on the screen
+ * put the "AI's" response on the screen. When the mouse is hovering
+ * over an interactable object (with the tag "Interactable"), they
+ * will be able to generate an AI prompt related to the object they
+ * are hovering over (which, in this case, would be looking at, since
+ * the mouse is constantly in the middle of the screen. hopefully)
  ********************************************************************/
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class fourInteract : MonoBehaviour
 {
-    public bool canInteract;
+    public Material highlightMaterial;
+    public Material selectionMaterial;
+
+    private Material originalMaterial;
+    private Transform highlight; // internal, only the computer will realize what is highlighted and what is
+                                 // not because I'm probably not going to learn how to give an object an outline
+    private Transform selection;
+    private RaycastHit raycastHit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
+        hoveringProcess();
 
-        }
+        interactionProcess();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,8 +52,58 @@ public class fourInteract : MonoBehaviour
 
     }
 
-    private void processInputs()
+    private void hoveringProcess()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        if (highlight != null)
+        {
+            //highlight.GetComponent<MeshRenderer>().material = originalMaterial;
+            highlight = null;
+        }
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+        {
+            highlight = raycastHit.transform;
+            if (highlight.CompareTag("Interactable") && highlight != selection)
+            {
+                //Debug.Log("Hovering over interactable object");
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+    }
+
+    private void interactionProcess()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetKey(KeyCode.E) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (selection != null)
+            {
+                //selection.GetComponent<MeshRenderer>().material = originalMaterial;
+                selection = null;
+            }
+
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+            {
+                selection = raycastHit.transform;
+                if (selection.CompareTag("Interactable"))
+                {
+                    processInput();
+                }
+                else
+                {
+                    selection = null;
+                }
+            }
+        }
+    }
+
+    private void processInput()
+    {
+        Debug.Log("Attempt to interact successful");
     }
 }
