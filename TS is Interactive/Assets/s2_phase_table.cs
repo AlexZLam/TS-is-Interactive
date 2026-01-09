25c65366e20e15ec11131bfc0a6906dc1c003f92
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-01-09 08:21:13 -0800

    click works and gamemanager phases

---
+++ TS is Interactive/Assets/s2_phase_table.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class s2_phase_table : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    readyup from desk interact: 
+    (on desk: book, paintbrush, knitting, plant) all give responses
+    readyUp after 3 clicked
+    
+    
+
+
+     */
+}
