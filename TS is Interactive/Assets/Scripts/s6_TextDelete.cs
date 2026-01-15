9252412a19e0dad06cfa7ae355c8f4ee1efe628b
codingGrease <jackson.leclaire@digipen.edu>
2026-01-15 07:11:22 -0800

    get rid of thaty

--- TS is Interactive/Assets/Scripts/s6_TextDelete.cs
+++ TS is Interactive/Assets/Scripts/s6_TextDelete.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class s6_TextDelete : MonoBehaviour
 {
     private float timer = 0;
-    //public GameObject me;
+    private s6_GameManager manager;
 
+
+    private void Start()
+    {
+        manager.obj
+    }
     // Update is called once per frame
     void Update()
     {
